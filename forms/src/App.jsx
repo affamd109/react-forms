ed1c0fd25283c05e65bba6428d80302f58ea2f99
affamd109 <affaque109@gmail.com>
2025-05-29 16:16:07 +0530

    forms

--- forms/src/App.jsx
+++ forms/src/App.jsx
@@ -1,4 +1,3 @@
-import './App.css'
 import UsernameForm from './UsernameForm'
 
 function App() {
@@ -6,7 +5,7 @@ function App() {
 
   return (
     <>
-    <h1 className='text-4xl text-red-500 mb-15'>hello</h1>
+    <h1 className='text-4xl text-red-500 mb-15 '>React forms</h1>
 
     <UsernameForm/>
 
--- forms/src/UsernameForm.jsx
+++ forms/src/UsernameForm.jsx
@@ -3,10 +3,17 @@ import { useState } from "react"
 export default function UsernameForm(){
     const [username , setUsername] = useState("");
 
+    const updateUsername = (e) => {
+        console.log("Changed")
+
+        setUsername(e.target.value)
+    }
+
     return (
         <div>
-            <input type="text" placeholder="username" value={username} />
-            <button>Sumbit</button>
+            <label htmlFor="username" >Enter a username</label>
+            <input id="username" className="border-2 border-gray-400 p-1 m-1 rounded-md" type="text" placeholder="Username" value={username} onChange={updateUsername} />
+            <button className="border-2 border-gray-400 p-1 rounded-md hover:opacity-70 cursor-pointer" >Submit</button>
         </div>
     )
 }
