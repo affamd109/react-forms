1c8d54f95375b5d2b9e892b12b02910c28de0619
affamd109 <affaque109@gmail.com>
2025-05-29 14:14:58 +0530

    initial commit

---
+++ forms/src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+import UsernameForm from './UsernameForm'
+
+function App() {
+
+
+  return (
+    <>
+    <h1 className='text-4xl text-red-500 mb-15'>hello</h1>
+
+    <UsernameForm/>
+
+
+     
+    </>
+  )
+}
+
+export default App
---
+++ forms/src/UsernameForm.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react"
+
+export default function UsernameForm(){
+    const [username , setUsername] = useState("");
+
+    return (
+        <div>
+            <input type="text" placeholder="username" value={username} />
+            <button>Sumbit</button>
+        </div>
+    )
+}
